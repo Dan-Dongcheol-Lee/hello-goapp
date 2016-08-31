dd2e59f207c1d1ca1885a5fd3605efbd1e0864ec
Dan Lee <dan.lee@papercut.com>
2016-08-31 13:51:51 +1000

    Rename package hello

--- hellofoo/app/app.go
+++ hellofoo/app/app.go
@@ -4,11 +4,11 @@ import (
     "net/http"
 
     "log"
-    "github.com/Dan-Dongcheol-Lee/hello-goapp/hellofoo/hello"
+    "github.com/Dan-Dongcheol-Lee/hello-goapp/hellofoo/api"
 )
 
 func init() {
     log.Println("Called init()")
-    http.HandleFunc("/hello", hello.Hello)
-    http.HandleFunc("/trace-foo", hello.TraceFoo)
+    http.HandleFunc("/hello", api.Hello)
+    http.HandleFunc("/trace-foo", api.TraceFoo)
 }
